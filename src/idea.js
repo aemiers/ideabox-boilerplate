7c471155939c41891874ffc3bb016cbc467b1e27
Jahara Clark <jaharalewis@hotmail.com>
2020-10-29 10:50:46 -1000

    Update main/idea js.

--- src/idea.js
+++ src/idea.js
@@ -1,7 +1,22 @@
 class Idea {
-  constructor(title, body) {
+  constructor(title, body, star) {
   this.id = Date.now();
   this.title = title;
   this.body = body;
+  this.star = false;
   }
+
+  function saveToStorage() {
+
+  }
+
+  function deleteFromStorage() {
+
+
+  }
+
+  function updateIdea() {
+
+  }
+  
 }
